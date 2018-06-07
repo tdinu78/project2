42ab73d79e3679b47a0c02d5d3fb31e947c68c6a
tdinu <tdinu78@gmail.com>
2018-06-07 06:28:35 +0300

    add /index,/resource permissions

--- src/main/java/com/social/config/WebSecurityConfig.java
+++ src/main/java/com/social/config/WebSecurityConfig.java
@@ -17,6 +17,8 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.Arrays;
+
 import static com.social.security.SecurityConstants.SIGN_UP_URL;
 
 @Configuration
@@ -35,8 +37,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable().authorizeRequests()
-                .antMatchers(HttpMethod.POST, SIGN_UP_URL, "/", "/resources/**", "/registration",
-                        "/index").permitAll()
+                .antMatchers(SIGN_UP_URL, "/", "/resources/**",
+                        "/registration", "/index").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .addFilter(new JWTAuthenticationFilter(authenticationManager()))
