1ee3507cff025ad0fe6018ff0229fcfd1e575d67
tdinu <tdinu78@gmail.com>
2018-06-10 01:02:17 +0300

    add /index,/resource permissions

---
+++ src/main/java/com/social/web/FrontController.java
@@ -0,0 +1,52 @@
+package com.social.web;
+
+import com.social.model.Menus;
+import com.social.model.User;
+import com.social.repository.MenuRepository;
+import com.social.repository.UserRepository;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.*;
+
+@RestController
+@Controller
+public class FrontController {
+    @Autowired
+    MenuRepository menuRepository;
+
+    @RequestMapping(value="/api/askmenu", params="menuType")
+    public void askMenu(@RequestParam("menuType") String menuType, HttpServletResponse resp) {
+        List<Menus> menus = menuRepository.fetchByType(menuType);
+        JSONArray jsonArray = new JSONArray();
+        JSONObject jsonObject;
+        Map resMap;
+        for (Menus oneMenu: menus) {
+            resMap = new HashMap();
+            resMap.put("name", oneMenu.getMenu_name());
+            resMap.put("priority", oneMenu.getPosition());
+            jsonObject = new JSONObject(resMap);
+            jsonArray.put(jsonObject);
+        }
+        resMap = new HashMap();
+        resMap.put("rc", 0);
+        resMap.put("message", "OK");
+        resMap.put("results",jsonArray);
+        jsonObject = new JSONObject(resMap);
+        try {
+            resp.getWriter().print(jsonObject);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        resp.setContentType("application/json");
+    }
+}
